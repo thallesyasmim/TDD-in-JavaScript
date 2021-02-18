593e22ca9abec98808269961ea50f9127eb6c07e
thallesyasmim <thallesgabriel1307@gmail.com>
2021-02-18 09:30:00 -0300

    Business rule

---
+++ src/fizzbuzz/fizzbuzz.js
@@ -0,0 +1,21 @@
+function fizzbuzz(number) {
+    if(number !== 0) {
+        if(number % 3 === 0 && number % 5 === 0) {
+            return 'FizzBuzz'
+        }
+
+        if(number % 3 === 0) {
+            return 'Fizz'
+        }
+    
+        if(number % 5 === 0) {
+            return 'Buzz'
+        }
+    }
+
+    
+
+    return number
+}
+
+module.exports = fizzbuzz
