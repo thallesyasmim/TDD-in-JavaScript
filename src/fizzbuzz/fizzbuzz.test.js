447e1b552e4f57a21e5067ccb9efdbaa94fca953
thallesyasmim <thallesgabriel1307@gmail.com>
2021-02-18 09:29:42 -0300

    FizzBuzz Test Perfect

---
+++ src/fizzbuzz/fizzbuzz.test.js
@@ -0,0 +1,25 @@
+const { expect } = require('@jest/globals')
+const fizzbuzz = require('./fizzbuzz')
+
+describe('FizzBuzz Puzzle', () => {
+    test('If it is not a multiple of 3 or 5, return it yourself', () => {
+        const result = fizzbuzz(0)
+        expect(result).toEqual(0)
+    })
+
+    test('If it is a multiple of 3, return "Fizz"', () => {
+        const result = fizzbuzz(9)
+        expect(result).toEqual('Fizz')
+    })
+
+
+    test('If it is a multiple of 5, return "Buzz"', () => {
+        const result = fizzbuzz(10)
+        expect(result).toEqual('Buzz')
+    })
+
+    test('If it is a multiple of 3 and 5, return "FizzBuzz"', () => {
+        const result = fizzbuzz(15)
+        expect(result).toEqual('FizzBuzz')
+    })
+})
